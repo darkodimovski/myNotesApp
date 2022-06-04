b0da39aeb1baa74ba2624dc1d1101aa99ab9f785
Darko Dimovski <darkodimovski57@gmail.com>
2022-06-04 11:11:20 +0200

    running test

---
+++ src/components/__tests__/app.test.js
@@ -0,0 +1,18 @@
+import { render, screen } from '@testing-library/react'
+import App from '../../App';
+
+import Search from '../Search';
+
+
+test('should render search', () => {
+    render(<Search />);
+    const searchElement = screen.getByPlaceholderText(/Search.../i);
+    expect(searchElement).toBeInTheDocument();
+})
+
+
+test('notes app title render search', () => {
+    render(<App />);
+    const titleElement = screen.getByTestId('title');
+    expect(titleElement).toBeInTheDocument();
+})
